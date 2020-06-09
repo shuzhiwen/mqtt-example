764184ffd94f2c42962bdaf7f158b48c781d9670
shuzhiwen <448875211@qq.com>
2020-06-09 10:38:41 +0800

    local

--- src/sendMsg.js
+++ src/sendMsg.js
@@ -1,37 +1,40 @@
 const fs = require('fs');
 const {
-    exec
+  exec
 } = require('child_process');
 
+//接收来自终端设备的 MQTT 订阅
 setTimeout(() => {
-    exec(`mosquitto_sub -t outTopic > data.txt &`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`error: ${error}`);
-            return;
-        } else {
-            console.log(`stdout: ${stdout}`);
-            console.error(`stderr: ${stderr}`);
-        }
-    });
+  exec(`nohup mosquitto_sub -t uploadToServer > data.txt 2>&1 &`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error}`);
+      return;
+    } else {
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    }
+  });
 }, 0);
 
+//每隔一秒推送最新数据
 setInterval(() => {
-    fs.readFile('./data.txt', {
-        encoding: 'utf-8'
-    }, (error, data) => {
+  fs.readFile('./data.txt', {
+    encoding: 'utf-8'
+  }, (error, data) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    data = data.split('\n');
+    exec(`mosquitto_pub -t downloadFromServer -m "${data[data.length - 1]}"`,
+      (error, stdout, stderr) => {
         if (error) {
-            console.log(`error: ${error.message}`);
-            return;
+          console.error(`error: ${error}`);
+          return;
+        } else {
+          console.log(`stdout: ${stdout}`);
+          console.error(`stderr: ${stderr}`);
         }
-        data = data.split('\n');
-        exec(`echo ${data[data.length - 1]}`, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`error: ${error}`);
-                return;
-            } else {
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
-            }
-        });
-    });
+      });
+  });
 }, 1000);
