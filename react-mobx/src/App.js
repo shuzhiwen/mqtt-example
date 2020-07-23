1db5e221dcd021b326c90d5650298dfe4636ba09
yuwen <yuwen@yuwendeMacBook-Air.local>
2020-07-23 15:57:48 +0800

    local

--- react-mobx/src/App.js
+++ react-mobx/src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
-import './App.css';
+import React from "react";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+import "./App.css";
 
 @observer
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          123
-      </header>
+        <header className="App-header">123</header>
       </div>
     );
   }
