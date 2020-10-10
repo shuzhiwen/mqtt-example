8e64e33015ed862da441ed57e022c91f4c7637cc
三文 <>
2020-10-10 15:09:15 +0800

    mock 文字数据

--- threejs-example/src/app.js
+++ threejs-example/src/app.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import * as THREE from 'three';
+import poetry from './data/poetry.json';
 import { WEBGL } from './common/webgl';
-import { texts, images } from './data/news.json';
+import { images } from './data/news.json';
 import Modal, { Creator } from './components/modal';
 import './app.css';
 
+// 网站空间模型
 const modal = new Modal.Create();
 
 export default function App() {
-  modal.bindMouse({ angle: 60 });
-  modal.bindScroll({ distance: 20, z: true });
-  modal.drawTimeLine({ start: 2000, end: 2020, distance: 300 });
-
-  // 约束随机坐标范围
-  const [ base, length ] = [ 100, 100 ];
+  const [ base, length ] = [ 100, 400 ];
+  const [ yearStart, yearEnd, yearDistance ] = [ 2000, 2020, 300 ];
   const xRange = new Array(length).fill(base).map((v, i) => (i < length / 2 ? v + i : -v - i + length / 2));
   const yRange = new Array(length).fill(base).map((v, i) => (i < length / 2 ? v + i : -v - i + length / 2));
 
+  modal.bindMouse({ angle: 60 });
+  modal.bindScroll({ distance: 20, z: true });
+  modal.drawTimeLine({ start: yearStart, end: yearEnd, yearDistance });
+
   // 添加文字
-  texts.forEach(({ year, month, news }) => {
-    const x = xRange[Math.floor(Math.random() * 200)];
-    const y = yRange[Math.floor(Math.random() * 200)];
-    const z = (year + month / 12 - 2000) * 300;
+  poetry.forEach(({ paragraphs }) => {
+    const x = xRange[Math.floor(Math.random() * length)];
+    const y = yRange[Math.floor(Math.random() * length)];
+    const z = Math.floor(Math.random() * (yearEnd - yearStart)) * yearDistance;
     const ray = new THREE.Vector3(x, y, 0);
     const angleY = ray.angleTo(new THREE.Vector3(0, 1, 0));
 
@@ -29,7 +31,7 @@ export default function App() {
     const { object } = Creator.createText({
       position: { x, y, z },
       rotation: { x: 0, y: Math.abs(angleY - Math.PI / 2) * (x > 0 ? -1 : 1), z: 0 },
-      text: news,
+      text: paragraphs[Math.floor(Math.random() * paragraphs.length)],
       size: 20,
       width: 400,
       height: 100
@@ -40,9 +42,9 @@ export default function App() {
 
   // 添加图片
   images.forEach(({ year, month, source }) => {
-    const x = xRange[Math.floor(Math.random() * 200)];
-    const y = yRange[Math.floor(Math.random() * 200)];
-    const z = (year + month / 12 - 2000) * 300;
+    const x = xRange[Math.floor(Math.random() * length)];
+    const y = yRange[Math.floor(Math.random() * length)];
+    const z = (year + month / 12 - 2000) * yearDistance;
     const ray = new THREE.Vector3(x, y, 0);
     const angleY = ray.angleTo(new THREE.Vector3(0, 1, 0));
 
@@ -66,6 +68,7 @@ export default function App() {
     document.getElementById('container').appendChild(warning);
   }
 
+  // 绘制全局动态背景
   return (
     <div className="container">
       <div id="stars" />
